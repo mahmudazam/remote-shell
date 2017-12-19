1581740233b132a81eb159dabf3590e7ef80eaa6
Mahmud Azam (mfa256) <mfa256@mail.usask.ca>
2017-12-18 21:38:15 -0600

    Refactor to set up for piping

--- src/exec/mod.rs
+++ src/exec/mod.rs
@@ -1,69 +1,91 @@
 
-use std::process::Command;
+use std::process::{Command};
+use std::fs;
+use std::os::unix::fs::PermissionsExt;
 
 use built_ins::run_built_in;
 
 static PATH : [&'static str; 4] =
     ["/bin/", "/usr/bin/", "/usr/local/bin/", "/usr/sbin/"];
 
-fn try_exec(name : &String, argv : &Vec<String>) -> i32 {
-    for i in PATH.iter() {
-        /* Try an exec: */
-        let p = format!("{}{}", i, name);
-        let child = Command::new(p)
-                            .args(argv)
-                            .spawn();
-        match child {
-            Ok(mut c) => {
-                let exit_status = c.wait()
-                    .expect("Wait failure");
-                return match exit_status.code() {
-                    None => -1,
-                    Some(s) => s,
-                };
-            },
-            Err(_) => {
-                ; /* Continue if failed */
-            },
-        }
-    }
-
-    if name.len() != 0 {
-        println!("-shell: command not found");
-    }
-    return -1;
-}
-
 fn path_and_args(comm_str : &str) -> (String, Vec<String>) {
-    let comm_vec : Vec<&str> = comm_str.split_whitespace().collect();
+    let comm_vec : Vec<&str> = comm_str
+        .trim()
+        .split_whitespace()
+        .collect();
+
     let name = String::from(match comm_vec.get(0) {
         None => "",
         Some(s) => s,
     });
+
     let argv : Vec<String> = match comm_vec.get(1..comm_vec.len()) {
         None => Vec::new(),
-        Some(a) => a.iter().map(
-            |x| String::from(*x)
-        ).collect(),
+        Some(a) => a.iter()
+            .map(|x|
+                String::from(*x)
+            ).collect(),
     };
     
     return (name, argv);
 }
 
-fn get_comms(comm : String) -> Vec<(String, Vec<String>)> {
+fn get_comm_strings(comm : String) -> Vec<(String, Vec<String>)> {
     comm.split('|').map(path_and_args).collect()
+}
+
+fn which(name : &String) -> Option<String> {
+    for i in PATH.iter() {
+        let p = format!("{}{}", i, name);
+        match fs::metadata(&p) {
+            Ok(m) => {
+                if m.is_file() // regular file
+                    && m.permissions().mode() & 0o111 != 0 { // executable
+                    return Some(p);
+                } else {
+                    ; // continue
+                }
+            },
+            Err(_) => {
+                ; // continue
+            },
+        }
+    }
+    return None;
+}
+
+fn run_command(commands : Vec<(String, Vec<String>)>) -> i32 {
+    let path = match which(&(commands[0].0)) {
+        None => {
+          return -1;
+        },
+        Some(s) => s,
+    };
+    let child = Command::new(&path)
+        .args(&(commands[0].1)).spawn();
+    match child {
+        Ok(mut c) => {
+            let exit_status = c.wait()
+                .expect("Wait failure");
+            match exit_status.code() {
+                None => -1,
+                Some(s) => s,
+            }
+        },
+        Err(_) => -1,
+    }
 }
 
 pub fn exec_comm(comm : String) -> i32 {
     /* Tokenize string and get the command name and arguments: */
-    let command = get_comms(comm);
+    let command = get_comm_strings(comm);
 
     /* Try running as a built-in: */
     let exit_status = run_built_in(&(command[0].0), &(command[0].1));
 
     /* Not a built-in: */
     if -1 == exit_status {
-        return try_exec(&(command[0].0), &(command[0].1));
+        return run_command(command);
     } else {
         return 0;
     }
